b792dea1719b72848ea43e2bed5a2731f89d95ab
Jonah Daniel Hocum <40188857+jdh33@users.noreply.github.com>
2024-08-11 21:59:04 +0000

    build: add logic to update conda environment

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -7,19 +7,19 @@ apt-get install --yes libgl1 ghostscript python3-tk
 # create environment in the project's directory using a prefix
 ENV_PREFIX="venv"
 ENV_DIR="${PWD}/${ENV_PREFIX}"
-if ! test -d "${ENV_DIR}"; then
+if ! test -d "$ENV_DIR"; then
     conda env create --file environment.yml --prefix "./${ENV_PREFIX}" --quiet --yes
-    conda env list
 else
-    conda env list
+    conda env update --file environment.yml --prefix "./${ENV_PREFIX}" --quiet
 fi
+conda env list
 
 # create environment in the default conda environment directory
 # with the name provided in the yaml file
 # ENV_NAME=$(grep "name:" environment.yml | sed 's/name: //')
-# if ! { conda env list | grep "${ENV_NAME}"; } >/dev/null 2>&1; then
-#     conda env create -f environment.yml
-#     conda env list
+# if ! { conda env list | grep "$ENV_NAME"; } >/dev/null 2>&1; then
+#     conda env create --file environment.yml --quiet --yes
 # else
-#     conda env list
+#     conda env update --file environment.yml --name "$ENV_NAME" --quiet
 # fi
+# conda env list
